24c0324896ae3a10636c3b9daa38686258ac8596
Abid Rahman <abidur@intouchsol.com>
2013-06-15 13:35:45 -0500

    i am done

---
+++ Sherlock/Hackathon.Sherlock/Hackathon.Sherlock.Web/GameHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Hackathon.Sherlock.Web
+{
+    public class GameHub : Hub
+    {
+        public void SendChallenge(string challenge)
+        {
+
+            Clients.All.challenge("test");
+        }
+
+        public void SendUserResponse(string sessionId, string response)
+        {
+            Clients.All.sendResponse(sessionId,response);
+        }
+
+        public void GetSessionId()
+        {
+            var context = HttpContext.Current;
+            var sessionId = context.Session.SessionID;
+           // var client=Clients.Client(Context.ConnectionId);
+        }
+    }
+}   
--- Sherlock/Hackathon.Sherlock/Hackathon.Sherlock.Web/Global.asax.cs
+++ Sherlock/Hackathon.Sherlock/Hackathon.Sherlock.Web/Global.asax.cs
@@ -16,6 +16,7 @@ namespace Hackathon.Sherlock.Web
     {
         protected void Application_Start()
         {
+            RouteTable.Routes.MapHubs();
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
