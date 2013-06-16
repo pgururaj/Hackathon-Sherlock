aed380a35ac584e030e6ce188e3b1cef88616c1d
Phaneesh <phaneesh@gmail.com>
2013-06-16 02:13:06 -0500

    esaas

---
+++ Sherlock/Hackathon.Sherlock/Hackathon.AlchemyWorker.Console/AlchemyPaylaod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.AlchemyWorker.Console
+{
+    public class AlchemyPaylaod
+    {
+        public string Url { get; set; }
+        public string Category { get; set; }
+    }
+}
