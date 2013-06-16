1ab22249dce32f10e7655d7c63f968665df3f85b
Phaneesh <phaneesh@gmail.com>
2013-06-15 22:23:41 -0500

    Iron IO code

---
+++ Sherlock/Hackathon.Sherlock/Hackathon.Sherlock.IronIO/Models/QueueTaskRequest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Sherlock.IronIO.Models
+{
+    public class QueueTaskRequest
+    {
+        public class Task
+        {
+            public string code_name { get; set; }
+            public string payload { get; set; }
+        }
+
+        public IList<Task> tasks { get; set; }
+    }
+
+    public class QueueTaskResponse
+    {
+        public class Task
+        {
+            public string id { get; set; }
+        }
+        public IList<Task> tasks { get; set; }
+    }
+
+    public class TaskInfo
+    {
+        public string id { get; set; }
+        public string project_id { get; set; }
+        public string code_id { get; set; }
+        public string code_history_id { get; set; }
+        public string status { get; set; }
+        public string code_name { get; set; }
+        public string code_rev { get; set; }
+        public string start_time { get; set; }
+        public string end_time { get; set; }
+        public string duration { get; set; }
+        public string timeout { get; set; }
+        public string payload { get; set; }
+        public string updated_at { get; set; }
+        public string created_at { get; set; }
+    }
+
+
+    
+}
---
+++ Sherlock/Hackathon.Sherlock/Hackathon.Sherlock.IronIO/QueueTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Sherlock.IronIO
+{
+    public static class QueueTask
+    {
+        public static string queue_task(string projectId, string token, string worker)
+        {
+            string uri = "https://worker-aws-us-east-1.iron.io:443/2/projects/" + projectId + "/tasks";
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.ContentType = "application/json";
+            request.Headers.Add("Authorization", "OAuth " + token);
+            request.UserAgent = "IronMQ .Net Client";
+            request.Method = "POST";
+            // We hand code the JSON payload here. You can automatically convert it, if you prefer
+            string body = "{\"tasks\": [ { \"code_name\": \"" + worker + "\", \"payload\": \"{\\\"key\\\": \\\"value\\\", \\\"fruits\\\": [\\\"apples\\\", \\\"oranges\\\"]}\"} ] }";
+            if (body != null)
+            {
+                using (System.IO.StreamWriter write = new System.IO.StreamWriter(request.GetRequestStream()))
+                {
+                    write.Write(body);
+                    write.Flush();
+                }
+            }
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(response.GetResponseStream()))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        static public void Main(string[] args)
+        {
+            Console.WriteLine(queue_task("51bbe549ed3d7679f5000282", "BXxvffaWJeFwM4WTo52mt1x9OXY", "smart.worker"));
+        }
+    }
+}
