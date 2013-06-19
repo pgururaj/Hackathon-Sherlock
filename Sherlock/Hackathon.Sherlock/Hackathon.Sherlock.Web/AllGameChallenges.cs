fbc109865b4207f4eada85dd60974169339ccb0e
Abid Rahman <abidur@intouchsol.com>
2013-06-18 23:55:50 -0500

    egr

--- Sherlock/Hackathon.Sherlock/Hackathon.Sherlock.Web/AllGameChallenges.cs
+++ Sherlock/Hackathon.Sherlock/Hackathon.Sherlock.Web/AllGameChallenges.cs
@@ -16,7 +16,7 @@ namespace Hackathon.Sherlock.Web
          new GameRound { Category = Category.Person, Challenge="This person co-founded Microsoft with Bill Gates", CorrectResponse="Paul Allen", Reward=200, Used=false } ,
          new GameRound { Category = Category.Person, Challenge="This person is known as the fastest man on earth", CorrectResponse="Usain Bolt", Reward=200, Used=false } ,
          //new GameRound { Category = Category.Person, Challenge="This place known as the city of fountains", CorrectResponse="Kansas City", Reward=200, Used=false } ,
-         new GameRound { Category = Category.City, Challenge="This small middle eastern city has the tallest building in the world", CorrectResponse="Dubai", Reward=400, Used=false } ,
+         new GameRound { Category = Category.City, Challenge="This Arabian Gulf Emirate has the tallest building in the world", CorrectResponse="Dubai", Reward=400, Used=false } ,
          new GameRound { Category = Category.City, Challenge="This city is named after Romulus and Remus", CorrectResponse="Rome" , Reward=200, Used=false} ,
          new GameRound { Category = Category.City, Challenge="This place is the capital of New Zealand", CorrectResponse="Wellington", Reward=800, Used=false }
          ,new GameRound { Category = Category.City, Challenge="The \"Hand of God\" goal happened in a soccer world cup held this latin American city", CorrectResponse="Mexico City", Reward=800, Used=false }
